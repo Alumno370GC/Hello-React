1ed1f929f25c25cc68ad7effaac42df680b3ca2f
Alumno370GC <alumnoitleon@gmail.com>
2024-06-17 13:38:49 -0600

    Hello-React

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () =>{
+return (
+<div>
+<h2> Hola mundo</h2>
+<h2> Luis Alberto </h2>
+</div>
+
+);
+
+}
+
+export  default Header;  //me salio error por poner -> ()
+
